3919a4c294020c803c366cacbd99760fbbd3b343
Swen Schillig <swen@vnet.ibm.com>
2015-11-23 16:06:27 +0100

    FSAL_GPFS[fsal_share.c]: Introduce doxygen comments and minor cleanup.
    Doxygen comments are added to the file- and
    functions-headers. In addition the code is cleaned-up.
    
    Change-Id: I9112cd02ffe763accd6b210fab506f5ae2003da2
    Signed-off-by: Swen Schillig <swen@vnet.ibm.com>

--- src/FSAL/FSAL_GPFS/fsal_share.c
+++ src/FSAL/FSAL_GPFS/fsal_share.c
@@ -1,4 +1,6 @@
-/*
+/**
+ * @file fsal_share.c
+ *
  * Copyright IBM Corporation, 2010
  *  Contributor: Aneesh Kumar K.v  <aneesh.kumar@linux.vnet.ibm.com>
  *
@@ -20,47 +22,48 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
  *
- *
  */
-#include "config.h"
 
 #include "fsal.h"
 #include "fsal_internal.h"
 #include "fsal_convert.h"
 
-/**
- * GPFSFSAL_share_op:
+/** @fn fsal_status_t
+ *	GPFSFSAL_share_op(int mntfd, int fd, void *owner, fsal_share_param_t req_share)
+ *  @param mntfd Mount file descriptor
+ *  @param owner Owner
+ *  @param req_share Request share
+ *
+ *  @return Status of operation
  */
-fsal_status_t GPFSFSAL_share_op(int mntfd,
-				int fd,
-				void *p_owner,
-				fsal_share_param_t request_share)
+fsal_status_t
+GPFSFSAL_share_op(int mntfd, int fd, void *owner, fsal_share_param_t req_share)
 {
-	int rc = 0;
-	struct share_reserve_arg share_arg;
+	struct share_reserve_arg share_arg = {0};
 	int errsv = 0;
+	int rc = 0;
 
 	LogFullDebug(COMPONENT_FSAL,
 		     "Share reservation: access:%u deny:%u owner:%p",
-		     request_share.share_access, request_share.share_deny,
-		     p_owner);
+		     req_share.share_access, req_share.share_deny, owner);
 
 	share_arg.mountdirfd = mntfd;
 	share_arg.openfd = fd;
-	share_arg.share_access = request_share.share_access;
-	share_arg.share_deny = request_share.share_deny;
+	share_arg.share_access = req_share.share_access;
+	share_arg.share_deny = req_share.share_deny;
 
 	rc = gpfs_ganesha(OPENHANDLE_SHARE_RESERVE, &share_arg);
 	errsv = errno;
 
-	if (rc < 0) {
-		LogDebug(COMPONENT_FSAL,
-			 "gpfs_ganesha: OPENHANDLE_SHARE_RESERVE returned error, rc=%d, errno=%d",
-			 rc, errsv);
-		if (errsv == EUNATCH)
-			LogFatal(COMPONENT_FSAL, "GPFS Returned EUNATCH");
-		return fsalstat(posix2fsal_error(errsv), errsv);
-	}
+	if (rc >= 0)
+		return fsalstat(ERR_FSAL_NO_ERROR, 0);
 
-	return fsalstat(ERR_FSAL_NO_ERROR, 0);
+	/* error */
+	LogDebug(COMPONENT_FSAL,
+		 "gpfs_ganesha: OPENHANDLE_SHARE_RESERVE returned error, rc=%d, errno=%d",
+		 rc, errsv);
+	if (errsv == EUNATCH)
+		LogFatal(COMPONENT_FSAL, "GPFS Returned EUNATCH");
+	return fsalstat(posix2fsal_error(errsv), errsv);
+
 }
