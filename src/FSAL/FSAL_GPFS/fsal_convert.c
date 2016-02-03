6099aa0a915a82e3fffb06e6590110924938c1ec
Swen Schillig <swen@vnet.ibm.com>
2016-02-03 15:56:09 +0100

    FSAL_GPFS[fsal_convert.c]: Minor cleanup.
    Minor code cleanup.
    
    FSF - moved doxygen fixup to doxygen patch.
    
    Change-Id: I9c51eae0b75353ee05cb6ea7bdeaac73bf6d708a
    Signed-off-by: Swen Schillig <swen@vnet.ibm.com>
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/FSAL/FSAL_GPFS/fsal_convert.c
+++ src/FSAL/FSAL_GPFS/fsal_convert.c
@@ -35,27 +35,28 @@ fsal_status_t
 gpfsfsal_xstat_2_fsal_attributes(gpfsfsal_xstat_t *gpfs_buf,
 				 struct attrlist *fsal_attr, bool use_acl)
 {
-	struct stat *p_buffstat;
+	struct stat *buf;
 
-	/* sanity checks */
 	if (!gpfs_buf || !fsal_attr)
 		return fsalstat(ERR_FSAL_FAULT, 0);
 
-	p_buffstat = &gpfs_buf->buffstat;
+	buf = &gpfs_buf->buffstat;
 
-	LogDebug(COMPONENT_FSAL, "inode %ld", p_buffstat->st_ino);
+	LogDebug(COMPONENT_FSAL, "inode %ld", buf->st_ino);
 
 	/* Fills the output struct */
 	if (FSAL_TEST_MASK(fsal_attr->mask, ATTR_TYPE)) {
-		fsal_attr->type = posix2fsal_type(p_buffstat->st_mode);
+		fsal_attr->type = posix2fsal_type(buf->st_mode);
 		LogFullDebug(COMPONENT_FSAL, "type = 0x%x",
 			     fsal_attr->type);
 	}
+
 	if (FSAL_TEST_MASK(fsal_attr->mask, ATTR_SIZE)) {
-		fsal_attr->filesize = p_buffstat->st_size;
+		fsal_attr->filesize = buf->st_size;
 		LogFullDebug(COMPONENT_FSAL, "filesize = %llu",
 			     (unsigned long long)fsal_attr->filesize);
 	}
+
 	if (FSAL_TEST_MASK(fsal_attr->mask, ATTR_FSID)) {
 		fsal_attr->fsid = gpfs_buf->fsal_fsid;
 		LogFullDebug(COMPONENT_FSAL,
@@ -63,6 +64,7 @@ gpfsfsal_xstat_2_fsal_attributes(gpfsfsal_xstat_t *gpfs_buf,
 			     fsal_attr->fsid.major,
 			     fsal_attr->fsid.minor);
 	}
+
 	if (FSAL_TEST_MASK(fsal_attr->mask, ATTR_ACL)) {
 		fsal_attr->acl = NULL;
 		if (use_acl && gpfs_buf->attr_valid & XATTR_ACL) {
@@ -72,159 +74,152 @@ gpfsfsal_xstat_2_fsal_attributes(gpfsfsal_xstat_t *gpfs_buf,
 		}
 		LogFullDebug(COMPONENT_FSAL, "acl = %p", fsal_attr->acl);
 	}
+
 	if (FSAL_TEST_MASK(fsal_attr->mask, ATTR_FILEID)) {
-		fsal_attr->fileid = (uint64_t) (p_buffstat->st_ino);
+		fsal_attr->fileid = (uint64_t) (buf->st_ino);
 		LogFullDebug(COMPONENT_FSAL, "fileid = %" PRIu64,
 			     fsal_attr->fileid);
 	}
 
 	if (FSAL_TEST_MASK(fsal_attr->mask, ATTR_MODE)) {
-		fsal_attr->mode = unix2fsal_mode(p_buffstat->st_mode);
+		fsal_attr->mode = unix2fsal_mode(buf->st_mode);
 		LogFullDebug(COMPONENT_FSAL, "mode = %"PRIu32,
 			     fsal_attr->mode);
 	}
+
 	if (FSAL_TEST_MASK(fsal_attr->mask, ATTR_NUMLINKS)) {
-		fsal_attr->numlinks = p_buffstat->st_nlink;
+		fsal_attr->numlinks = buf->st_nlink;
 		LogFullDebug(COMPONENT_FSAL, "numlinks = %u",
 			     fsal_attr->numlinks);
 	}
+
 	if (FSAL_TEST_MASK(fsal_attr->mask, ATTR_OWNER)) {
-		fsal_attr->owner = p_buffstat->st_uid;
+		fsal_attr->owner = buf->st_uid;
 		LogFullDebug(COMPONENT_FSAL, "owner = %" PRIu64,
 			     fsal_attr->owner);
 	}
+
 	if (FSAL_TEST_MASK(fsal_attr->mask, ATTR_GROUP)) {
-		fsal_attr->group = p_buffstat->st_gid;
+		fsal_attr->group = buf->st_gid;
 		LogFullDebug(COMPONENT_FSAL, "group = %" PRIu64,
 			     fsal_attr->group);
 	}
+
 	if (FSAL_TEST_MASK(fsal_attr->mask, ATTR_ATIME)) {
 		fsal_attr->atime =
-		    posix2fsal_time(p_buffstat->st_atime,
-				    p_buffstat->st_atim.tv_nsec);
+		    posix2fsal_time(buf->st_atime, buf->st_atim.tv_nsec);
 		LogFullDebug(COMPONENT_FSAL, "atime = %lu",
 			     fsal_attr->atime.tv_sec);
 	}
 
 	if (FSAL_TEST_MASK(fsal_attr->mask, ATTR_CTIME)) {
 		fsal_attr->ctime =
-		    posix2fsal_time(p_buffstat->st_ctime,
-				    p_buffstat->st_ctim.tv_nsec);
+		    posix2fsal_time(buf->st_ctime, buf->st_ctim.tv_nsec);
 		LogFullDebug(COMPONENT_FSAL, "ctime = %lu",
 			     fsal_attr->ctime.tv_sec);
 	}
+
 	if (FSAL_TEST_MASK(fsal_attr->mask, ATTR_MTIME)) {
 		fsal_attr->mtime =
-		    posix2fsal_time(p_buffstat->st_mtime,
-				    p_buffstat->st_mtim.tv_nsec);
+		    posix2fsal_time(buf->st_mtime, buf->st_mtim.tv_nsec);
 		LogFullDebug(COMPONENT_FSAL, "mtime = %lu",
 			     fsal_attr->mtime.tv_sec);
 	}
 
 	if (FSAL_TEST_MASK(fsal_attr->mask, ATTR_CHGTIME)) {
-		if (p_buffstat->st_mtime == p_buffstat->st_ctime) {
-			if (p_buffstat->st_mtim.tv_nsec >
-			    p_buffstat->st_ctim.tv_nsec)
+		if (buf->st_mtime == buf->st_ctime) {
+			if (buf->st_mtim.tv_nsec > buf->st_ctim.tv_nsec)
 				fsal_attr->chgtime =
-				    posix2fsal_time(p_buffstat->st_mtime,
-						    p_buffstat->st_mtim.
-						    tv_nsec);
+				    posix2fsal_time(buf->st_mtime,
+						    buf->st_mtim.tv_nsec);
 			else
 				fsal_attr->chgtime =
-				    posix2fsal_time(p_buffstat->st_ctime,
-						    p_buffstat->st_ctim.
-						    tv_nsec);
-		} else if (p_buffstat->st_mtime > p_buffstat->st_ctime) {
-			fsal_attr->chgtime =
-			    posix2fsal_time(p_buffstat->st_mtime,
-					    p_buffstat->st_mtim.tv_nsec);
+				    posix2fsal_time(buf->st_ctime,
+						    buf->st_ctim.tv_nsec);
+		} else if (buf->st_mtime > buf->st_ctime) {
+			fsal_attr->chgtime = posix2fsal_time(buf->st_mtime,
+							buf->st_mtim.tv_nsec);
 		} else {
-			fsal_attr->chgtime =
-			    posix2fsal_time(p_buffstat->st_ctime,
-					    p_buffstat->st_ctim.tv_nsec);
+			fsal_attr->chgtime = posix2fsal_time(buf->st_ctime,
+							buf->st_ctim.tv_nsec);
 		}
-		fsal_attr->change =
-		    (uint64_t) fsal_attr->chgtime.tv_sec +
-		    (uint64_t) fsal_attr->chgtime.tv_nsec;
+		fsal_attr->change = (uint64_t) fsal_attr->chgtime.tv_sec +
+				    (uint64_t) fsal_attr->chgtime.tv_nsec;
+
 		LogFullDebug(COMPONENT_FSAL, "chgtime = %lu",
 			     fsal_attr->chgtime.tv_sec);
 
 	}
 
 	if (FSAL_TEST_MASK(fsal_attr->mask, ATTR_SPACEUSED)) {
-		fsal_attr->spaceused = p_buffstat->st_blocks * S_BLKSIZE;
+		fsal_attr->spaceused = buf->st_blocks * S_BLKSIZE;
 		LogFullDebug(COMPONENT_FSAL, "spaceused = %llu",
 			     (unsigned long long)fsal_attr->spaceused);
 	}
 
 	if (FSAL_TEST_MASK(fsal_attr->mask, ATTR_RAWDEV)) {
-		fsal_attr->rawdev = posix2fsal_devt(p_buffstat->st_rdev);
+		fsal_attr->rawdev = posix2fsal_devt(buf->st_rdev);
 		LogFullDebug(COMPONENT_FSAL, "rawdev major = %u, minor = %u",
 			     (unsigned int)fsal_attr->rawdev.major,
 			     (unsigned int)fsal_attr->rawdev.minor);
 	}
-
-	/* everything has been copied ! */
 
 	return fsalstat(ERR_FSAL_NO_ERROR, 0);
 }
 
 /* Covert GPFS NFS4 ACLs to FSAL ACLs, and set the ACL
  * pointer of attribute. */
-static int gpfs_acl_2_fsal_acl(struct attrlist *p_object_attributes,
-			       gpfs_acl_t *p_gpfsacl)
+static int gpfs_acl_2_fsal_acl(struct attrlist *obj_attr, gpfs_acl_t *gpfs_acl)
 {
-	fsal_acl_status_t status;
-	fsal_acl_data_t acldata;
-	fsal_ace_t *pace;
-	fsal_acl_t *pacl;
-	gpfs_ace_v4_t *pace_gpfs;
+	fsal_acl_status_t acl_status = 0;
+	fsal_acl_data_t acl_data = {0};
+	fsal_ace_t *ace;
+	fsal_acl_t *acl;
+	gpfs_ace_v4_t *ace_gpfs;
 
-	/* sanity checks */
-	if (!p_object_attributes || !p_gpfsacl)
+	if (!obj_attr || !gpfs_acl)
 		return ERR_FSAL_FAULT;
 
+	/* everything has been copied ! */
 	/* Create fsal acl data. */
-	acldata.naces = p_gpfsacl->acl_nace;
-	acldata.aces = (fsal_ace_t *) nfs4_ace_alloc(acldata.naces);
+	acl_data.naces = gpfs_acl->acl_nace;
+	acl_data.aces = (fsal_ace_t *) nfs4_ace_alloc(acl_data.naces);
 
 	/* Fill fsal acl data from gpfs acl. */
-	for (pace = acldata.aces, pace_gpfs = p_gpfsacl->ace_v4;
-	     pace < acldata.aces + acldata.naces; pace++, pace_gpfs++) {
-		pace->type = pace_gpfs->aceType;
-		pace->flag = pace_gpfs->aceFlags;
-		pace->iflag = pace_gpfs->aceIFlags;
-		pace->perm = pace_gpfs->aceMask;
+	for (ace = acl_data.aces, ace_gpfs = gpfs_acl->ace_v4;
+		ace < acl_data.aces + acl_data.naces; ace++, ace_gpfs++) {
+		ace->type = ace_gpfs->aceType;
+		ace->flag = ace_gpfs->aceFlags;
+		ace->iflag = ace_gpfs->aceIFlags;
+		ace->perm = ace_gpfs->aceMask;
 
-		if (IS_FSAL_ACE_SPECIAL_ID(*pace)) { /* Record special user. */
-			pace->who.uid = pace_gpfs->aceWho;
-		} else {
-			if (IS_FSAL_ACE_GROUP_ID(*pace))
-				pace->who.gid = pace_gpfs->aceWho;
-			else	/* Record user. */
-				pace->who.uid = pace_gpfs->aceWho;
-		}
+		if (IS_FSAL_ACE_SPECIAL_ID(*ace)) /* Record special user. */
+			ace->who.uid = ace_gpfs->aceWho;
+		else if (IS_FSAL_ACE_GROUP_ID(*ace))
+			ace->who.gid = ace_gpfs->aceWho;
+		else    /* Record user. */
+			ace->who.uid = ace_gpfs->aceWho;
 
 		LogMidDebug(COMPONENT_FSAL,
-			 "gpfs_acl_2_fsal_acl: fsal ace: type = 0x%x, flag = 0x%x, perm = 0x%x, special = %d, %s = 0x%x",
-			 pace->type, pace->flag, pace->perm,
-			 IS_FSAL_ACE_SPECIAL_ID(*pace),
-			 GET_FSAL_ACE_WHO_TYPE(*pace), GET_FSAL_ACE_WHO(*pace));
+			    "gpfs_acl_2_fsal_acl: fsal ace: type = 0x%x, flag = 0x%x, perm = 0x%x, special = %d, %s = 0x%x",
+			    ace->type, ace->flag, ace->perm,
+			    IS_FSAL_ACE_SPECIAL_ID(*ace),
+			    GET_FSAL_ACE_WHO_TYPE(*ace),
+			    GET_FSAL_ACE_WHO(*ace));
 	}
 
 	/* Create a new hash table entry for fsal acl. */
-	pacl = nfs4_acl_new_entry(&acldata, &status);
-	LogMidDebug(COMPONENT_FSAL, "fsal acl = %p, fsal_acl_status = %u", pacl,
-		 status);
+	acl = nfs4_acl_new_entry(&acl_data, &acl_status);
+	LogMidDebug(COMPONENT_FSAL, "fsal acl = %p, fsal_acl_status = %u", acl,
+		    acl_status);
 
-	if (pacl == NULL) {
+	if (acl == NULL) {
 		LogCrit(COMPONENT_FSAL,
 			"gpfs_acl_2_fsal_acl: failed to create a new acl entry");
-		return ERR_FSAL_FAULT;
+	return ERR_FSAL_FAULT;
 	}
 
-	/* Add fsal acl to attribute. */
-	p_object_attributes->acl = pacl;
+	obj_attr->acl = acl;  /* Add fsal acl to attribute. */
 
 	return ERR_FSAL_NO_ERROR;
 }
@@ -243,51 +238,44 @@ fsal_status_t
 fsal_acl_2_gpfs_acl(struct fsal_obj_handle *dir_hdl, fsal_acl_t *fsal_acl,
 		    gpfsfsal_xstat_t *gpfs_buf)
 {
+	gpfs_acl_t *gpfs_acl = (gpfs_acl_t *) gpfs_buf->buffacl;
+	fsal_ace_t *ace;
 	int i;
-	fsal_ace_t *pace;
-	gpfs_acl_t *p_gpfsacl;
 
-	p_gpfsacl = (gpfs_acl_t *) gpfs_buf->buffacl;
+	gpfs_acl->acl_level = 0;
+	gpfs_acl->acl_version = GPFS_ACL_VERSION_NFS4;
+	gpfs_acl->acl_type = GPFS_ACL_TYPE_NFS4;
+	gpfs_acl->acl_nace = fsal_acl->naces;
+	gpfs_acl->acl_len = offsetof(gpfs_acl_t, ace_v1) +
+				gpfs_acl->acl_nace * sizeof(gpfs_ace_v4_t);
 
-	p_gpfsacl->acl_level = 0;
-	p_gpfsacl->acl_version = GPFS_ACL_VERSION_NFS4;
-	p_gpfsacl->acl_type = GPFS_ACL_TYPE_NFS4;
-	p_gpfsacl->acl_nace = fsal_acl->naces;
-	p_gpfsacl->acl_len =
-	    ((int)(signed long)&(((gpfs_acl_t *) 0)->ace_v1)) +
-	    p_gpfsacl->acl_nace * sizeof(gpfs_ace_v4_t);
+	for (ace = fsal_acl->aces, i = 0;
+	     ace < (fsal_acl->aces + fsal_acl->naces); ace++, i++) {
+		gpfs_acl->ace_v4[i].aceType = ace->type;
+		gpfs_acl->ace_v4[i].aceFlags = ace->flag;
+		gpfs_acl->ace_v4[i].aceIFlags = ace->iflag;
+		gpfs_acl->ace_v4[i].aceMask = ace->perm;
 
-	for (pace = fsal_acl->aces, i = 0;
-	     pace < fsal_acl->aces + fsal_acl->naces; pace++, i++) {
-		p_gpfsacl->ace_v4[i].aceType = pace->type;
-		p_gpfsacl->ace_v4[i].aceFlags = pace->flag;
-		p_gpfsacl->ace_v4[i].aceIFlags = pace->iflag;
-		p_gpfsacl->ace_v4[i].aceMask = pace->perm;
-
-		if (IS_FSAL_ACE_SPECIAL_ID(*pace))
-			p_gpfsacl->ace_v4[i].aceWho = pace->who.uid;
-		else {
-			if (IS_FSAL_ACE_GROUP_ID(*pace))
-				p_gpfsacl->ace_v4[i].aceWho = pace->who.gid;
+		if (IS_FSAL_ACE_SPECIAL_ID(*ace)) {
+			gpfs_acl->ace_v4[i].aceWho = ace->who.uid;
+		} else {
+			if (IS_FSAL_ACE_GROUP_ID(*ace))
+				gpfs_acl->ace_v4[i].aceWho = ace->who.gid;
 			else
-				p_gpfsacl->ace_v4[i].aceWho = pace->who.uid;
+				gpfs_acl->ace_v4[i].aceWho = ace->who.uid;
 		}
 
 		LogMidDebug(COMPONENT_FSAL,
-			 "fsal_acl_2_gpfs_acl: gpfs ace: type = 0x%x, flag = 0x%x, perm = 0x%x, special = %d, %s = 0x%x",
-			 p_gpfsacl->ace_v4[i].aceType,
-			 p_gpfsacl->ace_v4[i].aceFlags,
-			 p_gpfsacl->ace_v4[i].aceMask,
-			 (p_gpfsacl->ace_v4[i].
-			  aceIFlags & FSAL_ACE_IFLAG_SPECIAL_ID) ? 1 : 0,
-			 (p_gpfsacl->ace_v4[i].
-			  aceFlags & FSAL_ACE_FLAG_GROUP_ID) ? "gid" : "uid",
-			 p_gpfsacl->ace_v4[i].aceWho);
+			    "fsal_acl_2_gpfs_acl: gpfs ace: type = 0x%x, flag = 0x%x, perm = 0x%x, special = %d, %s = 0x%x",
+			    ace->type, ace->flag, ace->perm,
+			    (ace->iflag & FSAL_ACE_IFLAG_SPECIAL_ID) ? 1 : 0,
+			    (ace->flag & FSAL_ACE_FLAG_GROUP_ID) ? "gid" :
+								   "uid",
+			    gpfs_acl->ace_v4[i].aceWho);
 
 		/* It is invalid to set inherit flags on non dir objects */
 		if (dir_hdl->type != DIRECTORY &&
-		    (p_gpfsacl->ace_v4[i].aceFlags &
-		    FSAL_ACE_FLAG_INHERIT) != 0) {
+		    (gpfs_acl->ace_v4[i].aceFlags & FSAL_ACE_FLAG_INHERIT)) {
 			LogMidDebug(COMPONENT_FSAL,
 			   "attempt to set inherit flag to non dir object");
 			return fsalstat(ERR_FSAL_INVAL, 0);
@@ -295,99 +283,14 @@ fsal_acl_2_gpfs_acl(struct fsal_obj_handle *dir_hdl, fsal_acl_t *fsal_acl,
 
 		/* It is invalid to set inherit only with
 		 * out an actual inherit flag */
-		if ((p_gpfsacl->ace_v4[i].aceFlags & FSAL_ACE_FLAG_INHERIT) ==
+		if ((gpfs_acl->ace_v4[i].aceFlags & FSAL_ACE_FLAG_INHERIT) ==
 			FSAL_ACE_FLAG_INHERIT_ONLY) {
 			LogMidDebug(COMPONENT_FSAL,
 			   "attempt to set inherit only without an inherit flag");
 			return fsalstat(ERR_FSAL_INVAL, 0);
 		}
-
 	}
 
 	return fsalstat(ERR_FSAL_NO_ERROR, 0);
 }
 
-fsal_status_t fsal_cred_2_gpfs_cred(struct user_cred *p_fsalcred,
-				    struct xstat_cred_t *p_gpfscred)
-{
-	int i;
-
-	if (!p_fsalcred || !p_gpfscred)
-		return fsalstat(ERR_FSAL_FAULT, 0);
-
-	p_gpfscred->principal = p_fsalcred->caller_uid;
-	p_gpfscred->group = p_fsalcred->caller_gid;
-	p_gpfscred->num_groups = p_fsalcred->caller_glen;
-
-	for (i = 0; i < p_fsalcred->caller_glen; i++)
-		p_gpfscred->eGroups[i] = p_fsalcred->caller_garray[i];
-
-	return fsalstat(ERR_FSAL_NO_ERROR, 0);
-}
-
-fsal_status_t fsal_mode_2_gpfs_mode(mode_t fsal_mode, fsal_accessflags_t v4mask,
-				    unsigned int *p_gpfsmode, bool is_dir)
-{
-	int gpfs_mode = 0;
-
-	if (!p_gpfsmode)
-		return fsalstat(ERR_FSAL_FAULT, 0);
-
-	/* If mode is zero, translate v4mask to posix mode. */
-	if (fsal_mode == 0) {
-		if (!is_dir) {
-			if (v4mask & FSAL_ACE_PERM_READ_DATA)
-				gpfs_mode |= FSAL_R_OK;
-		} else {
-			if (v4mask & FSAL_ACE_PERM_LIST_DIR) {
-				gpfs_mode |= FSAL_R_OK;
-				gpfs_mode |= FSAL_X_OK;
-			}
-		}
-
-		if (!is_dir) {
-			if (v4mask & FSAL_ACE_PERM_WRITE_DATA)
-				gpfs_mode |= FSAL_W_OK;
-		} else {
-			if (v4mask & FSAL_ACE_PERM_ADD_FILE) {
-				gpfs_mode |= FSAL_W_OK;
-				gpfs_mode |= FSAL_X_OK;
-			}
-		}
-
-		if (!is_dir) {
-			if (v4mask & FSAL_ACE_PERM_APPEND_DATA)
-				gpfs_mode |= FSAL_W_OK;
-		} else {
-			if (v4mask & FSAL_ACE_PERM_ADD_SUBDIRECTORY) {
-				gpfs_mode |= FSAL_W_OK;
-				gpfs_mode |= FSAL_X_OK;
-			}
-		}
-
-		if (!is_dir) {
-			if (v4mask & FSAL_ACE_PERM_EXECUTE)
-				gpfs_mode |= FSAL_X_OK;
-		} else {
-			if (v4mask & FSAL_ACE_PERM_DELETE_CHILD) {
-				gpfs_mode |= FSAL_W_OK;
-				gpfs_mode |= FSAL_X_OK;
-			}
-		}
-
-		if (v4mask & FSAL_ACE_PERM_DELETE)
-			gpfs_mode |= FSAL_W_OK;
-
-		gpfs_mode = gpfs_mode >> 24;
-	} else {
-		gpfs_mode = fsal_mode >> 24;
-	}
-
-	LogMidDebug(COMPONENT_FSAL,
-		 "fsal_mode 0x%x, v4mask 0x%x, is_dir %d converted to gpfs_mode 0x%x",
-		 fsal_mode, v4mask, is_dir, gpfs_mode);
-
-	*p_gpfsmode = gpfs_mode;
-
-	return fsalstat(ERR_FSAL_NO_ERROR, 0);
-}
